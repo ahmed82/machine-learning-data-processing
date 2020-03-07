a5e1beaf9947af7136e4d7bb5370b81b319cd98e
Ahmed Al-Salih <ahmed.al-salih@delhaize.com>
2020-03-07 12:54:45 -0500

    replace the lables with numbers encoding

--- data_preprocessing_template.py
+++ data_preprocessing_template.py
@@ -10,8 +10,13 @@ dataset = pd.read_csv('Data.csv')
 x = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 3].values
 
-# Splitting the dataset into the Training set and Test set
+# Taking care of missing data
 from sklearn.preprocessing import Imputer
 imputer = Imputer(missing_values="NaN", strategy="mean", axis=0, verbose=0, copy=True)
 imputer = imputer.fit(x[:, 1:3])
 x[:, 1:3] = imputer.transform(x[:, 1:3])
+
+#Encoding categoerical data
+from sklearn.preprocessing import LabelEncoder
+LabelEncoder_x = LabelEncoder()
+x[:, 0] = LabelEncoder_x.fit_transform(x[:, 0])
