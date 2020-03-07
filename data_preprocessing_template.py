e3d323a66227468e77b8921bbecdf307dffa757e
Ahmed Al-Salih <ahmed.al-salih@delhaize.com>
2020-03-07 12:23:06 -0500

    find and replace a missing value with the mean

--- data_preprocessing_template.py
+++ data_preprocessing_template.py
@@ -7,5 +7,11 @@ import pandas as pd
 
 # Importing the dataset
 dataset = pd.read_csv('Data.csv')
-X = dataset.iloc[:, :-1].values
+x = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 3].values
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values="NaN", strategy="mean", axis=0, verbose=0, copy=True)
+imputer = imputer.fit(x[:, 1:3])
+x[:, 1:3] = imputer.transform(x[:, 1:3])
